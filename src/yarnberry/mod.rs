22dd4deb2bc6a6256331dfd1b36447db88506f56
luis <luis@pleme.io>
2024-11-28 18:17:22 -0300

    add yarnberry tests

--- src/yarnberry/mod.rs
+++ src/yarnberry/mod.rs
@@ -1,3 +1,10 @@
+/*
+* YarnBerryEnvironment will be a structure representing all data possibly used
+* from the yarn berry envrionment.
+*
+* Various other structs will be attached to represent each element
+* in berry such as the package_json or yarnrc_yml
+*/
 pub mod package_json;
 // pub mod yarnrc_yml;
 // pub mod pnp;
